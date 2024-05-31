0660bc7cafe5bdca9122ef2c619a11bcc8f62dd3
maximiliano rojas <mrojas59@externos.bancoestado.cl>
2024-05-30 22:01:30 -0400

    create user | validadores en user.dto | findall user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const { password, ...userData } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const createUSer = new this.userModel({ ...userData, password: hashedPassword });
+    await createUSer.save();
+    return 'usuario creado con éxito';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<Omit<User, 'password'>[]> {
+    const users = await this.userModel.find().select('-password').exec();
+    return users;
   }
 
   findOne(id: number) {
